33edd3aabe90baf409dcbb7c5c788088ca274dc2
Ben <BenLorantfy@users.noreply.github.com>
2025-11-24 17:34:58 -0500

    Implement `getPathForInaccessibleForegroundColors` and `getPathForInaccessibleBackgroundColors`

--- src/utils.stories.tsx
+++ src/utils.stories.tsx
@@ -1,59 +1,194 @@
 import React from 'react';
-import { getPathForInaccessibleColors } from './utils';
-import { ColorArea } from './react-aria-components/ColorArea';
-import { ColorThumb } from './react-aria-components/ColorThumb';
-import { ColorSlider } from './react-aria-components/ColorSlider';
-import { ColorField } from './react-aria-components/ColorField';
+import { getPathForInaccessibleBackgroundColors, getPathForInaccessibleForegroundColors } from './utils';
+import { ColorArea } from '../react-aria-components/ColorArea';
+import { ColorSlider } from '../react-aria-components/ColorSlider';
+import { ColorField } from '../react-aria-components/ColorField';
 import { parseColor } from 'react-aria-components';
-import { ColorPicker } from './react-aria-components/ColorPicker';
+import { ColorPicker } from '../react-aria-components/ColorPicker';
+import Color from 'color';
+import { rgb } from 'wcag-contrast';
 
-const ExampleSVG = () => {
-  let [value, setValue] = React.useState(parseColor('hsl(25, 100%, 50%)'));
-  const pathD = getPathForInaccessibleColors({ hue: 210, backgroundColor: '#ffffff' });
-  const hue = value.getChannelValue("hue");
+function calculateSelectionColor(cursorColor: Parameters<typeof Color>[0]) {
+    const color = Color(cursorColor);
 
-  console.log(hue);
+    return Color({
+        h: color.hsl().hue(),
+        s: color.hsl().saturationl(),
+        l: Math.min(color.hsl().lightness() * 5, 90)
+    }).toString();
+}
+
+interface StoryProps {
+  requiredContrastRatio?: number;
+}
+
+const Story: React.FC<StoryProps> = ({ requiredContrastRatio = 4.5 }) => {
+  const [cursorColor, setCursorColor] = React.useState(parseColor('hsl(25, 100%, 25%)'));
+  const hue = cursorColor.getChannelValue("hue");
+  const inaccessibleCursorColorsPath = React.useMemo(
+    () => getPathForInaccessibleForegroundColors({ hue, backgroundColor: [255, 255, 255], requiredContrastRatio }),
+    [hue, requiredContrastRatio]
+  );
+  const inaccessibleSelectionColorsPath = React.useMemo(
+    () => getPathForInaccessibleBackgroundColors({ hue, foregroundColor: [0,0,0], requiredContrastRatio, transformColor: calculateSelectionColor }),
+    [hue, requiredContrastRatio]
+  );
+  const selectionColor = calculateSelectionColor(cursorColor.toString());
+
+  const cursorColorColor = Color(cursorColor.toString()).rgb();
+  const selectionColorColor = Color(selectionColor).rgb();
+
+  const cursorContrastWithWhiteBackground = rgb([cursorColorColor.red(), cursorColorColor.green(), cursorColorColor.blue()], [255,255,255]);
+  const textContrastWithSelectionColor = rgb([0,0,0], [selectionColorColor.red(), selectionColorColor.green(), selectionColorColor.blue()]);
+
+  const hasEnoughContrast = cursorContrastWithWhiteBackground > requiredContrastRatio && textContrastWithSelectionColor > requiredContrastRatio;
+
   return (
-    <div>
-      <div style={{ width: '192px', position: 'relative' }}>
-        <svg
-          width="100%"
-          height="100%"
-          viewBox="0 0 100 100"
-          style={{
-            display: "block",
-            position: "absolute",
-            top: 0,
-            left: 0,
-            zIndex: 1,
-            pointerEvents: 'none',
-          }}
-        >
-          <g transform="translate(0,0)">
-            <path d={pathD} fill="rgba(200, 0, 0, 0.2)" />
-          </g>
-        </svg>
-        <ColorArea
-          colorSpace="hsb"
-          xChannel="saturation"
-          yChannel="brightness"
-          value={value}
-          onChange={setValue}
-        />
+    <ColorPicker value={cursorColor} onChange={setCursorColor}>
+      <div style={{ display: 'flex', flexDirection: 'row', gap: '16px', width: '400px' }}>
+        <div style={{ display: 'flex', flexDirection: 'column', gap: '16px' }}>
+          <div style={{ width: '192px', position: 'relative' }}>
+            <svg
+              width="100%"
+              height="100%"
+              viewBox="0 0 100 100"
+              style={{
+                display: "block",
+                position: "absolute",
+                top: 0,
+                left: 0,
+                zIndex: 1,
+                pointerEvents: 'none',
+              }}
+            >
+              <defs>
+                <pattern
+                  id="diagonalStripes"
+                  patternUnits="userSpaceOnUse"
+                  width="10"
+                  height="10"
+                  patternTransform="rotate(45)"
+                >
+                  <rect
+                    x="0"
+                    y="0"
+                    width="5"
+                    height="10"
+                    fill={hasEnoughContrast ? "white" : "#E51010"}
+                    fillOpacity={hasEnoughContrast ? 0.35 : 0.5}
+                    style={{
+                      transition: "fill 0.3s, fill-opacity 0.3s"
+                    }}
+                  />
+                </pattern>
+              </defs>
+              <g transform="translate(0,0)">
+                <path 
+                  d={inaccessibleCursorColorsPath} 
+                  fill="url(#diagonalStripes)" 
+                  stroke={hasEnoughContrast ? "rgba(210,210,210)" : "#E51010"} 
+                  strokeWidth={2}
+                  style={{
+                    transition: "stroke 0.3s"
+                  }}
+                />
+                <path 
+                  d={inaccessibleSelectionColorsPath} 
+                  fill="url(#diagonalStripes)" 
+                  stroke={hasEnoughContrast ? "rgba(210,210,210)" : "#E51010"} 
+                  strokeWidth={2} 
+                  style={{
+                    transition: "stroke 0.3s"
+                  }}
+                />
+              </g>
+            </svg>
+            <ColorArea
+              colorSpace="hsl"
+              xChannel="saturation"
+              yChannel="lightness"
+              value={cursorColor}
+              onChange={setCursorColor}
+            />
+          </div>
+          <ColorSlider colorSpace="hsb" channel="hue" />
+          <ColorField label="Hex" value={cursorColor} />
+        </div>
+        <div style={{ display: 'flex', flexDirection: 'column', gap: '16px' }}>
+          <h2 style={{ marginTop: 0, marginBottom: 0}}>
+            Preview
+          </h2>
+          <div>
+            <span
+              style={{
+                backgroundColor: selectionColor,
+                borderRight: `4px solid ${cursorColor.toString()}`,
+                fontSize: '24px',
+                padding: '1px',
+                paddingRight: '3px',
+                display: 'inline-block',
+                position: 'relative',
+                userSelect: 'none',
+              }}
+            >
+              abc def ghi
+              <span
+                style={{
+                  position: 'absolute',
+                  top: '-8px',
+                  right: '-7px',
+                  width: '10px',
+                  height: '10px',
+                  backgroundColor: cursorColor.toString(),
+                  borderRadius: '50%',
+                  pointerEvents: 'none',
+                  display: 'block',
+                }}
+              />
+            </span>
+          </div>
+          {/* TODO: make this text fade-in */}
+          {!hasEnoughContrast && (
+            <div style={{
+              color: '#E51010',
+              fontSize: '20px'
+            }}>
+              { cursorContrastWithWhiteBackground < requiredContrastRatio ?
+            (
+              <>
+                Color is too light!<br /><br />
+                It may be hard to see the cursor.  Please select a darker color.
+              </>
+            ) : (
+              <>
+                Color is too dark!<br /><br />
+                It may be hard to see the text.  Please select a lighter color.
+              </>
+            )  
+            }
+            </div>
+          )}
+        </div>
       </div>
-      <ColorSlider colorSpace="hsb" channel="hue" value={value} onChange={setValue} />
-      <ColorField label="Hex" value={value} />
-    </div>
+    </ColorPicker>
   );
 };
 
-const meta = {
-  title: 'Utils/getPathForInaccessibleColors',
-  component: ExampleSVG,
+export default {
+  title: 'utils',
+  component: Story,
+  argTypes: {
+    requiredContrastRatio: {
+      control: { type: 'number', min: 1, max: 21, step: 0.1 },
+      defaultValue: 4.5,
+      description: 'The required contrast ratio for accessibility lines'
+    }
+  }
 };
 
-export default meta;
-
 export const Default = {
-  render: () => <ExampleSVG />,
+  render: (args: StoryProps) => <Story {...args} />,
+  args: {
+    requiredContrastRatio: 4.5,
+  },
 };
--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,79 @@
-export function getPathForInaccessibleColors({ hue, backgroundColor }: { hue: number, backgroundColor: string }) {
-    return `M0 0 H30 V30 H0 Z`;
+import { rgb } from 'wcag-contrast';
+import Color from 'color';
+
+type ColorInput = Parameters<typeof Color>[0];
+
+export function getPathForInaccessibleForegroundColors({ 
+    hue, 
+    backgroundColor,
+    requiredContrastRatio = 4.5
+}: { 
+    hue: number, 
+    backgroundColor: [r: number, g: number, b: number],
+    requiredContrastRatio: number
+}) {
+    if (backgroundColor[0] !== 255 || backgroundColor[1] !== 255 || backgroundColor[2] !== 255) {
+        throw new Error('Currently only white background is supported.');
+    }
+
+    const points: Array<{ x: number, y: number }> = [];
+    for (let saturation = 0; saturation <= 100; saturation++) {
+        points.push({
+            x: saturation,
+            y: 100 - findFirstForegroundLightnessThatIsInaccessible({ hue, saturation, backgroundColor, requiredContrastRatio }),
+        });
+    }
+    return `M0 0 ${points.map(point => `${point.x} ${point.y}`).join(' ')} 100 0 Z`;
+}
+
+export function getPathForInaccessibleBackgroundColors({ hue, foregroundColor, requiredContrastRatio = 4.5, transformColor }: { hue: number, foregroundColor: [r: number, g: number, b: number], requiredContrastRatio: number, transformColor?: (color: ColorInput) => string }) {
+    if (foregroundColor[0] !== 0 || foregroundColor[1] !== 0 || foregroundColor[2] !== 0) {
+        throw new Error('Currently only black foreground is supported.');
+    }
+
+    const points: Array<{ x: number, y: number }> = [];
+    for (let saturation = 0; saturation < 100; saturation++) {
+        const args: Parameters<typeof findFirstBackgroundLightnessThatIsInaccessible>[0] = {
+            hue,
+            saturation,
+            foregroundColor,
+            requiredContrastRatio,
+            ...(transformColor && { transformColor }),
+        };
+        points.push({
+            x: saturation,
+            y: 100 - findFirstBackgroundLightnessThatIsInaccessible(args),
+        });
+    }
+
+    return `M0 100 ${points.map(point => `${point.x} ${point.y}`).join(' ')} 100 100 Z`;
+}
+
+function findFirstForegroundLightnessThatIsInaccessible({ hue, saturation, backgroundColor, requiredContrastRatio }: { hue: number, saturation: number, backgroundColor: [r: number, g: number, b: number], requiredContrastRatio: number }) {
+    // TODO: figure out if we should start at 0 or 100
+    for (let lightness = 0; lightness <= 100; lightness++) {
+        const color = Color({ h: hue, s: saturation, l: lightness });
+        const contrast = rgb([color.red(), color.green(), color.blue()], backgroundColor);
+        const isInaccessible = contrast < requiredContrastRatio;
+        
+        if (isInaccessible) {
+            return lightness;
+        }
+    }
+    return 100;
+}
+
+function findFirstBackgroundLightnessThatIsInaccessible({ hue, saturation, foregroundColor, requiredContrastRatio, transformColor }: { hue: number, saturation: number, foregroundColor: [r: number, g: number, b: number], requiredContrastRatio: number, transformColor?: (color: ColorInput) => string }) {
+    // TODO: figure out if we should start at 0 or 100
+    for (let lightness = 100; lightness > 0; lightness--) {
+        const baseColor = { h: hue, s: saturation, l: lightness };
+        const transformedColor = transformColor ? transformColor(baseColor) : Color(baseColor).toString();
+        const color = Color(transformedColor).rgb();
+        const contrast = rgb(foregroundColor, [color.red(), color.green(), color.blue()])
+        const isInaccessible = contrast < requiredContrastRatio;
+        if (isInaccessible) {
+            return lightness;
+        }
+    }
+    return 100;
 }
